ea90bd7599e6c2850d7ed3de168df630792eb6d4
rafaelpeinado <rafael.peinado.silva@gmail.com>
2023-08-25 01:49:40 -0300

    feat: finaliza semana 1

---
+++ semana-1/Somador.py
@@ -0,0 +1,11 @@
+a = 10
+b = 20
+
+soma = a + b
+# assim não irá imprimir o valor, pois está imprimindo um texto string
+# print ("A soma dos números é soma") 
+
+# nesse caso demonstra que é para imprimir um texto e uma variável
+print ("A soma dos números é", soma)
+
+
